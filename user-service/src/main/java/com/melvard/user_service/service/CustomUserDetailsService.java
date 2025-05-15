80f197c6349bfdffb161200ed45667ac4efd5ed9
Melih Vardar <mvardar97@gmail.com>
2025-05-15 21:17:50 +0300

    CustomUserDetailsService added

---
+++ user-service/src/main/java/com/melvard/user_service/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.melvard.user_service.service;
+
+import com.melvard.user_service.entity.User;
+import com.melvard.user_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority(user.getRole().toString())));
+    }
+}
